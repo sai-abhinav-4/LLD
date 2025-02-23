3d4493d84adc7b9f817980f30a8ff89414d4e1eb
saiabhinav_endrapu <endrapu.saiabhinav@gmail.com>
2025-02-23 17:57:18 +0530

    initial project setup files

---
+++ src/main/java/com/practice/lld/low_level/LowLevelApplication.java
@@ -0,0 +1,13 @@
+package com.practice.lld.low_level;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LowLevelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LowLevelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/lld/low_level/LowLevelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.lld.low_level;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LowLevelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
